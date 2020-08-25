32053cab892d3cbbc0ad422779b2a01edeaa92ac
Christoph Weinsheimer <christoph.weinsheimer@esrlabs.com>
2020-08-25 20:21:39 +0200

    Fix mutt side format. Add bcp21 backup excludes.

--- .oh-my-zsh-custom/fzf.zsh
+++ .oh-my-zsh-custom/fzf.zsh
@@ -14,6 +14,3 @@ if type fd > /dev/null; then
   export FZF_DEFAULT_COMMAND='fd --type f'
   export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 fi
-
-# pacman search with preview
-alias pacsearch="pacman -Slq | fzf -m --preview 'pacman -Si {1}'"
